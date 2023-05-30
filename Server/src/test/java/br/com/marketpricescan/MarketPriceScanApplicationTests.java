24c81f17c5107cf81ea4e45765657a390e4d0584
e-magesk <dudamagesk@gmail.com>
2023-05-30 09:25:44 -0300

    Criei um projeto com Spring Boot

---
+++ Server/src/test/java/br/com/marketpricescan/MarketPriceScanApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marketpricescan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketPriceScanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
