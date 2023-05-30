cea468b968a8102729ec436ea6ee3d956afc67ce
e-magesk <dudamagesk@gmail.com>
2023-05-29 21:09:27 -0300

    Montando o projeto e aprendendo a usar o Android Studio

---
+++ app/src/main/java/br/com/marketpricescan/LoginActivity.kt.kt
@@ -0,0 +1,11 @@
+package br.com.marketpricescan
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.g)
+    }
+}
