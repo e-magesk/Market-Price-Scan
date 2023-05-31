0befd4f582b4136b54c8b121372e37178c6198c9
e-magesk <dudamagesk@gmail.com>
2023-05-30 21:14:46 -0300

    A tela de login redireciona para cadastro

--- Aplicativo-Android/app/src/main/java/br/com/marketpricescan/LoginActivity.kt
+++ Aplicativo-Android/app/src/main/java/br/com/marketpricescan/LoginActivity.kt
@@ -1,11 +1,21 @@
 package br.com.marketpricescan
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 
 class LoginActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+
+        val tvCadastrar = findViewById<TextView>(R.id.tvCadastrar)
+        tvCadastrar.setOnClickListener {
+            val intent = Intent(this, CadastroActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
