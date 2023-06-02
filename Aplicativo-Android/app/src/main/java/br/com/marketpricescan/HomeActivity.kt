c43de3e09782d25bdd80fca0c0c6ef4b5be11afd
e-magesk <dudamagesk@gmail.com>
2023-06-02 10:13:34 -0300

    O arquivo da home foi criado

---
+++ Aplicativo-Android/app/src/main/java/br/com/marketpricescan/HomeActivity.kt
@@ -0,0 +1,12 @@
+package br.com.marketpricescan
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class HomeActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home)
+    }
+}
