1cdcc2180d6ba2babf576fa3d3f53651f5011416
e-magesk <dudamagesk@gmail.com>
2023-06-06 22:46:00 -0300

    O email e a senha já estao sendo salvos

--- Aplicativo-Android/app/src/main/java/br/com/marketpricescan/CadastroActivity.kt
+++ Aplicativo-Android/app/src/main/java/br/com/marketpricescan/CadastroActivity.kt
@@ -6,7 +6,12 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.activity.ComponentActivity
+import androidx.compose.ui.graphics.Color
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 
 class CadastroActivity : ComponentActivity() {
 
@@ -24,21 +29,55 @@ class CadastroActivity : ComponentActivity() {
         etEmailCadastro  = findViewById(R.id.etEmailCadastro)
         etSenhaCadastro = findViewById(R.id.etSenhaCadastro)
 
-        Log.d("Debug","Quero cadastrar")
+//        Log.d("Debug","Quero cadastrar")
         btnCadastrar.setOnClickListener{view ->
-            Log.d("Debug","Estou cadastrando")
             val nome = etNomeCadastro.text.toString()
             val email = etEmailCadastro.text.toString()
             val senha = etSenhaCadastro.text.toString()
 
             if(nome.isEmpty() || email.isEmpty() || senha.isEmpty()) {
-                if(view != null) {
-                    Log.d("Debug", "Faltam parametros")
-                    Snackbar.make(btnCadastrar, "Preencha todos os campos", Snackbar.LENGTH_LONG)
-                        .show()
-                }
+                var snackbar = Snackbar.make(view, "Preencha todos os campos", Snackbar.LENGTH_LONG)
+                snackbar.setBackgroundTint(getColor(R.color.red))
+                snackbar.setTextColor(getColor(R.color.black))
+                snackbar.show()
+
             } else {
-                Log.d("Debug", view.toString())
+                CadastrarUsuario(view)
+            }
+        }
+    }
+
+    private fun CadastrarUsuario(view : View) {
+        val email = etEmailCadastro.text.toString()
+        val senha = etSenhaCadastro.text.toString()
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, senha)
+            .addOnCompleteListener { task ->
+            if(task.isSuccessful) {
+                var snackbar = Snackbar.make(view, "Usuário cadastrado com sucesso", Snackbar.LENGTH_LONG)
+                snackbar.setBackgroundTint(getColor(R.color.green))
+                snackbar.setTextColor(getColor(R.color.black))
+                snackbar.show()
+            }
+            else{
+                var erro : String = ""
+                try{
+                    throw task.exception!!
+                } catch (e : FirebaseAuthWeakPasswordException) {
+                    erro = "A senha deve conter no mínimo 6 caracteres"
+                } catch (e : FirebaseAuthUserCollisionException) {
+                    erro = "E-mail já cadastrado"
+                } catch (e : FirebaseAuthInvalidCredentialsException) {
+                    erro = "E-mail inválido"
+                } catch (e : Exception) {
+                    erro = "Erro ao cadastrar usuário"
+                }
+
+                var snackbar = Snackbar.make(view, erro, Snackbar.LENGTH_LONG)
+                snackbar.setBackgroundTint(getColor(R.color.red))
+                snackbar.setTextColor(getColor(R.color.black))
+                snackbar.show()
+
             }
         }
     }
