58eecf02c58f2554cef84f66a8a1d58464959a6b
e-magesk <dudamagesk@gmail.com>
2023-06-08 22:36:40 -0300

    Esqueleto da tela criado

---
+++ Aplicativo-Android/app/src/main/java/br/com/marketpricescan/ListaDeCompraActivity.kt
@@ -0,0 +1,12 @@
+package br.com.marketpricescan
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ListaDeCompraActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.lista_de_compra)
+    }
+}
