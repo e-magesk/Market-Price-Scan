fbc9a3c66c0d31e477fc731cdf67684502a9b153
e-magesk <dudamagesk@gmail.com>
2023-06-15 22:37:08 -0300

    Layout da home finalizado (por hora)

--- Aplicativo-Android/app/src/main/java/br/com/marketpricescan/ListaDeCompraActivity.kt
+++ Aplicativo-Android/app/src/main/java/br/com/marketpricescan/ListaDeCompraActivity.kt
@@ -1,9 +1,11 @@
 package br.com.marketpricescan
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ListView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import br.com.marketpricescan.util.ItemListaAdaptador
 
@@ -11,6 +13,7 @@ class ListaDeCompraActivity : AppCompatActivity() {
 
     private lateinit var lvListaDeCompra : ListView
     private lateinit var btnAdicionarItem : Button
+    private lateinit var tvListaVazia : TextView
     private lateinit var adaptador: ItemListaAdaptador
     var itens = mutableListOf<String>("Produto 1", "Produto 2", "Produto 3", "Produto 4", "Produto 5", "Produto 6", "Produto 7", "Produto 8", "Produto 9", "Produto 10", "Produto 11")
 
@@ -24,18 +27,19 @@ class ListaDeCompraActivity : AppCompatActivity() {
         lvListaDeCompra.adapter = adaptador
         lvListaDeCompra.isClickable = false
 
+        VerificarSituacaoLista()
+
         btnAdicionarItem.setOnClickListener(){view ->
             itens.add("")
             adaptador.itens = itens
             adaptador.notifyDataSetChanged()
             lvListaDeCompra.smoothScrollToPosition(adaptador.count - 1)
+            VerificarSituacaoLista()
 
         }
 
         lvListaDeCompra.setOnItemLongClickListener { parent, view, position, id ->
-            itens.removeAt(position)
-            adaptador.itens = itens
-            adaptador.notifyDataSetChanged()
+            PopUpConfirmacaoDeletarItem(itens[position], position)
             true
         }
     }
@@ -43,6 +47,36 @@ class ListaDeCompraActivity : AppCompatActivity() {
     private fun IniciarComponentes(){
         btnAdicionarItem = findViewById(R.id.btnAdicionarItem)
         lvListaDeCompra = findViewById(R.id.lvListaDeCompra)
+        tvListaVazia = findViewById(R.id.tvListaVazia)
+    }
+
+    private fun VerificarSituacaoLista(){
+        if(itens.size == 0){
+            tvListaVazia.visibility = TextView.VISIBLE
+            lvListaDeCompra.visibility = ListView.INVISIBLE
+        }
+        else{
+            tvListaVazia.visibility = TextView.INVISIBLE
+            lvListaDeCompra.visibility = ListView.VISIBLE
+        }
+    }
+
+    private fun PopUpConfirmacaoDeletarItem(item : String, position : Int){
+        val alertDialog = AlertDialog.Builder(this)
+            .setTitle("Deletar Item")
+            .setMessage("O item " + item + " será deletado da lista. Deseja continuar?")
+            .setPositiveButton("OK") { dialog, which ->
+                itens.removeAt(position)
+                adaptador.itens = itens
+                adaptador.notifyDataSetChanged()
+                VerificarSituacaoLista()
+                dialog.dismiss()
+            }
+            .setNegativeButton("Cancelar") { dialog, which ->
+                dialog.dismiss()
+            }
+            .create()
+        alertDialog.show()
     }
 
 }
