65418df602c636c1376742583c4bad3deba100fb
Eduarda Magesk <e-magesk>
2023-06-26 11:25:13 -0300

    Layout da pagina de edicao está pronta

---
+++ Aplicativo-Android/app/src/main/java/br/com/marketpricescan/EditarUsuarioActivity.kt
@@ -0,0 +1,11 @@
+package br.com.marketpricescan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditarUsuarioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.editar_usuario)
+    }
+}
