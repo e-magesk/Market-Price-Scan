32c0fa5986bd3eecb9ed0b014f79d133ab82d1a8
Leticia <leticia.c.rodrigues0@gmail.com>
2023-07-13 22:48:12 -0300

    Comentarios ListaDeCompraAdaptador

--- Aplicativo-Android/app/src/main/java/br/com/marketpricescan/util/ListaDeCompraAdaptador.kt
+++ Aplicativo-Android/app/src/main/java/br/com/marketpricescan/util/ListaDeCompraAdaptador.kt
@@ -18,6 +18,12 @@ import br.com.marketpricescan.model.ListaDeCompra
 import br.com.marketpricescan.model.Produto
 import com.google.firebase.firestore.FirebaseFirestore
 
+/**
+ * Adaptador personalizado para exibição de listas de compra em um RecyclerView.
+ *
+ * @property context Contexto da aplicação.
+ * @property listasDeCompra Lista de listas de compra a serem exibidas.
+ */
 class ListaDeCompraAdaptador(private val context : Context, private val listasDeCompra: MutableList<ListaDeCompra>) : RecyclerView.Adapter<ListaDeCompraAdaptador.ItemViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -34,6 +40,11 @@ class ListaDeCompraAdaptador(private val context : Context, private val listasDe
         return listasDeCompra.size
     }
 
+    /**
+     * ViewHolder para os itens da lista de compra.
+     *
+     * @param itemView A visualização de um item da lista de compra.
+     */
     inner class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val tvNomeListaDeCompra: TextView = itemView.findViewById(R.id.tvNomeListaDeCompra)
 
@@ -47,21 +58,25 @@ class ListaDeCompraAdaptador(private val context : Context, private val listasDe
             itemView.setOnClickListener {
                 val currentPosition = bindingAdapterPosition
                 notifyItemRangeChanged(currentPosition, 1, "selected")
-
-//                val bundle = Bundle()
-//                bundle.putParcelable("listaDeCompra", listasDeCompra[currentPosition])
-//                val intent = Intent(itemView.context, AtualizarListaDeCompraActivity::class.java)
-//                intent.putExtras(bundle)
-//                itemView.context.startActivity(intent)
-//                (itemView.context as? Activity)?.finish()
             }
 
         }
 
+        /**
+         * Liga os dados de uma lista de compra à visualização do item.
+         *
+         * @param lista Lista de compra a ser exibida.
+         */
         fun bind(lista: ListaDeCompra) {
             tvNomeListaDeCompra.setText(lista.nome)
         }
 
+        /**
+         * Exibe um pop-up de confirmação para deletar uma lista de compra.
+         *
+         * @param lista Lista de compra a ser deletada.
+         * @param position Posição da lista de compra na lista de exibição.
+         */
         private fun PopUpConfirmacaoDeletarLista(lista : ListaDeCompra, position : Int){
             val alertDialog = AlertDialog.Builder(itemView.context)
                 .setTitle("Deletar Item")
@@ -77,6 +92,12 @@ class ListaDeCompraAdaptador(private val context : Context, private val listasDe
             alertDialog.show()
         }
 
+        /**
+         * Deleta uma lista de compra do banco de dados.
+         *
+         * @param lista Lista de compra a ser deletada.
+         * @param position Posição da lista de compra na lista de exibição.
+         */
         private fun DeletarListaDeCompra(lista: ListaDeCompra, position: Int) {
             FirebaseFirestore.getInstance().collection("lista_de_compra")
                 .document(lista.id).delete()
